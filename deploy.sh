dff2a314a8604970f47eb41bfd1bf100f3add2ad
Nkem Emioma <lilymeche1983@icloud.com>
2022-08-22 04:59:20 +0000

    conflict 2 resolved

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 deploy java application
 deploy python apps
-deploy in the dev environment
+deploy in the dev env
 deeployment5 master
 deployment4
+deploy to staging env
+echo good job
+Tested and accepted
+
 
